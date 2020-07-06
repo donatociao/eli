b16c4ba9fcb52d3dd60ce67567ddea220d25d451
Donato <ciao.donatociao@gmail.com>
2020-07-06 17:40:02 +0200

    cortesia

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -2,7 +2,14 @@
 
 @section('content')
   @include('front.partials.nav')
-  @include('front.partials.slider')
+
+  <div class="container">
+    <div class="text-center mt-5 mb-5">
+      <h1>Sito in manutenzione</h1>
+      <h2>A breve saremo online con una nuova veste grafica!</h2>
+    </div>
+  </div>
+  {{-- @include('front.partials.slider')
   @include('front.partials.search-mobile')
   @include('front.partials.search-home')
 
@@ -18,7 +25,7 @@
 
   @if(count($news) > 0)
     @include('front.partials.news')
-  @endif
+  @endif --}}
 
   @include('front.partials.contatti')
   @include('front.partials.footer')
