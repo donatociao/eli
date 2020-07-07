70ecfa81a53be94356d3cf2c1adc29390f9f5b90
Donato <ciao.donatociao@gmail.com>
2020-07-07 11:01:42 +0200

    Revert "description fix"
    This reverts commit e9b5ee36031456834c1751ee518b16104107c71e.

--- app/Http/Controllers/ImmobiliController.php
+++ app/Http/Controllers/ImmobiliController.php
@@ -77,7 +77,6 @@ class ImmobiliController extends Controller
             'wc' => 'required|integer|min:0',
             'box' => 'required|integer|min:0',
             'ape' => 'required|string|max:255',
-            'description' => 'string',
             ];
 
         $customMessages = [
@@ -176,7 +175,6 @@ class ImmobiliController extends Controller
                 }
             }
         }
-        $nuovo_immobile->description = $request->description;
         $nuovo_immobile->save();
 
       if(!array_key_exists('evidenza', $dati_inseriti)) {
