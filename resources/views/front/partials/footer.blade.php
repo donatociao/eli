69307cc3d803796a5cf5adcba5d4dfc674d43afc
Donato <ciao.donatociao@gmail.com>
2020-07-08 13:49:36 +0200

    web dev fix

--- resources/views/front/partials/footer.blade.php
+++ resources/views/front/partials/footer.blade.php
@@ -5,7 +5,7 @@
         <p class="text-white">Eliano Servizi Immobiliari Soc. Coop. - P.IVA 05597730653 - <a href="{{route('privacy')}}">Privacy & Cookie Policy</a></p>
       </div>
       <div class="col-lg-4">
-        <p class="text-right text-white">web design | <a class="text-white" href="http://www.prelibato.eu/">Prelibato</a></p>
+        <p class="text-right text-white">web design & development | <a class="text-white font-weight-bold" href="http://www.prelibato.eu/">Prelibato</a></p>
       </div>
     </div>
   </div>
