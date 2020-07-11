66b0124566730c9d9f45f2065494dc1f379fded9
Donato <ciao.donatociao@gmail.com>
2020-07-11 12:37:19 +0200

    Revert "email template fix"
    This reverts commit 3b906d60c265e072e7144881408b0b2a3f77297b.

--- resources/views/email_template.blade.php
+++ resources/views/email_template.blade.php
@@ -1,3 +1,13 @@
+{{-- <p>Ciao Enrico,  {{ $message['name'] }}.</p>
+<p>Il suo messaggio è:</p>
+<p>{{ $message['message'] }}.</p>
+<br>
+<p>I suoi contatti sono:</p>
+<p><i class="fi-xnluxx-envelope"></i> {{ $message['email'] }}</p>
+<p><i class="fi-xnluxx-phone"></i> {{ $message['mobile'] }}</p>
+<br>
+<p>Buona giornata :)</p> --}}
+
 <h2>Ciao Enrico,</h2>
 <p>hai ricevuto una richiesta da : {{ $name }}</p>
 <p>Ecco i dettagli:</p>
