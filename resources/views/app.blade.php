4fb687b93c33a8c101500d694a9b023af0863bda
Donato <ciao.donatociao@gmail.com>
2020-07-13 01:04:24 +0200

    Add to screen Ios

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -56,7 +56,20 @@
             console.log('Laravel PWA: ServiceWorker registration failed: ', err);
         });
     }
-</script>
+
+    // Detects if device is on iOS
+    const isIos = () => {
+      const userAgent = window.navigator.userAgent.toLowerCase();
+      return /iphone|ipad|ipod/.test( userAgent );
+    }
+    // Detects if device is in standalone mode
+    const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+    // Checks if should display install popup notification:
+    if (isIos() && !isInStandaloneMode()) {
+      this.setState({ showInstallMessage: true });
+    }
+  </script>
     <!-- Hotjar Tracking Code for https://eliano.it -->
 <script>
     (function(h,o,t,j,a,r){
