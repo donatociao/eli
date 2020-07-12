7a00dc38d03d941b27d99552c2bb6b11dfbe284c
Donato <ciao.donatociao@gmail.com>
2020-07-12 23:22:29 +0200

    PWA testing

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,6 +12,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="x-ua-compatible" content="IE=edge" />
     <title>Eliano Immobiliare a Eboli</title>
+    @laravelPWA
     <!-- Hotjar Tracking Code for https://eliano.it -->
 <script>
     (function(h,o,t,j,a,r){
@@ -131,75 +132,9 @@
 
   <!-- Animate CSS -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
-  <!-- Web Application Manifest -->
-<link rel="manifest" href="/manifest.json">
-<!-- Chrome for Android theme color -->
-<meta name="theme-color" content="#000000">
 
-<!-- Add to homescreen for Chrome on Android -->
-<meta name="mobile-web-app-capable" content="yes">
-<meta name="application-name" content="PWA">
-<link rel="icon" sizes="512x512" href="/images/icons/icon-512x512.png">
-
-<!-- Add to homescreen for Safari on iOS -->
-<meta name="apple-mobile-web-app-capable" content="yes">
-<meta name="apple-mobile-web-app-status-bar-style" content="black">
-<meta name="apple-mobile-web-app-title" content="PWA">
-<link rel="apple-touch-icon" href="/images/icons/icon-512x512.png">
-
-<link href="/images/icons/splash-640x1136.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
-<link href="/images/icons/splash-750x1334.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
-<link href="/images/icons/splash-1242x2208.png" media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
-<link href="/images/icons/splash-1125x2436.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
-<link href="/images/icons/splash-828x1792.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
-<link href="/images/icons/splash-1242x2688.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
-<link href="/images/icons/splash-1536x2048.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
-<link href="/images/icons/splash-1668x2224.png" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
-<link href="/images/icons/splash-1668x2388.png" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
-<link href="/images/icons/splash-2048x2732.png" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
-
-<!-- Tile for Win8 -->
-<meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="/images/icons/icon-512x512.png">
-
-<script type="text/javascript">
-    // Initialize the service worker
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/serviceworker.js', {
-            scope: '.'
-        }).then(function (registration) {
-            // Registration was successful
-            console.log('Laravel PWA: ServiceWorker registration successful with scope: ', registration.scope);
-        }, function (err) {
-            // registration failed :(
-            console.log('Laravel PWA: ServiceWorker registration failed: ', err);
-        });
-    }
-</script>
 </head>
   <body>
-    {{-- <ul class="nav justify-content-between bg-eliano h-60 d-flex align-items-center sticky-top">
-      <li class="nav-item">
-        <a class="nav-link menu-open" href="#"><i class="fi-xwlux2-three-bars-solid text-white"></i></a>
-      </li>
-        @if (Route::has('login'))
-          @auth
-            <li class="nav-item">
-              <a class="nav-link text-white bold-20" href="{{ route('dash') }}">PANNELLO</a>
-            </li>
-          @else
-            <li class="nav-item">
-              <a class="nav-link text-white bold-20" href="{{ route('login') }}">LOGIN</a>
-            </li>
-
-            @if (Route::has('register'))
-              <li class="nav-item">
-                <a class="nav-link text-white bold-20" href="{{ route('register') }}">REGISTRATI</a>
-              </li>
-            @endif
-          @endauth
-        @endif
-      </ul> --}}
     @isset($messages)
       <script>Swal.fire({
               icon: '{{ $messages->msgType }}',
