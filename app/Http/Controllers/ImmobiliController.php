b22d7e59e3feb1631e28251fe88cb7687b24e238
Donato <ciao.donatociao@gmail.com>
2020-08-13 13:44:39 +0200

    Menu search fix

--- app/Http/Controllers/ImmobiliController.php
+++ app/Http/Controllers/ImmobiliController.php
@@ -31,7 +31,7 @@ class ImmobiliController extends Controller
         $cat = DB::table('categories')->get();
         $stato = array('Fittasi');
         $cities = DB::select("CALL getAvailCities()");
-        return view('front.fittasi', compact('matches', 'stato', 'cities','cat'));
+        return view('front.results', compact('matches', 'stato', 'cities','cat'));
     }
 
     public function indexVendesi()
@@ -40,7 +40,7 @@ class ImmobiliController extends Controller
         $cat = DB::table('categories')->get();
         $stato = array('Vendesi');
         $cities = DB::select("CALL getAvailCities()");
-        return view('front.fittasi', compact('matches', 'stato', 'cities','cat'));
+        return view('front.results', compact('matches', 'stato', 'cities','cat'));
     }
 
 
