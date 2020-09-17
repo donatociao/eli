fbcba5bfa9b353d1094267e44d5e8c7dcfa0d72f
Donato <ciao.donatociao@gmail.com>
2020-09-17 13:39:42 +0200

    Fix Dash 2

--- resources/views/back/partials/base.blade.php
+++ resources/views/back/partials/base.blade.php
@@ -8,13 +8,13 @@
               {{ session('status') }}
             </div>
           @endif
-        <a class="text-decoration-none" href="{{route('home')}}"><img src="{{asset('img/logo.png')}}" class="logo" alt="logo"></a>
+        <a  href="{{route('home')}}"><img src="{{asset('img/logo.png')}}" class="logo" alt="logo"></a>
         @if ($newMessages)
           <div class="mt-4 alert alert-danger text-center" role="alert">
             <h3>Hai un nuovo messaggio!</h3>
           </div>
         @endif
-        <a href="{{route('messages')}}"><button class="mt-4 btn btn-info d-flex justify-content-center"type="button" name="button"><h5>Messaggi</h5><i class="ml-2 far fa-2x fa-envelope"></i></button></a>
+        <a class="text-decoration-none" href="{{route('messages')}}"><button class="mt-4 btn btn-info d-flex justify-content-center"type="button" name="button"><h5>Vai ai messaggi</h5><i class="ml-2 far fa-2x fa-envelope"></i></button></a>
       </div>
     </div>
   </div>
